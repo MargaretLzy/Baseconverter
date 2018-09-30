12276e24005d012df46475d99e48ffa5b209f830
analogclock <greer.hoffmann@gmail.com>
2018-09-30 10:19:31 -0400

    Adding an interface for the project

---
+++ BaseConverterInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseconverterapp;
+
+/**
+ *
+ * @author greerhoffmann
+ */
+public interface BaseConverter {
+    
+    public String base10ToN();
+    public int baseNToBase10();
+    public String add();
+    public String subtract();
+    public String multiply();
+    public String divide();
+    
+}

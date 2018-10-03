d9610c9d3cdb2da56c94bdbfc947b0036323722d
analogclock <greer.hoffmann@gmail.com>
2018-10-02 20:10:12 -0400

    Modified again to add comments about method functions in the interface

--- BaseConverterApp/src/baseconverterapp/BaseConverter.java
+++ BaseConverterApp/src/baseconverterapp/BaseConverter.java
@@ -11,10 +11,39 @@ package baseconverterapp;
  */
 public interface BaseConverter {
     
+    /**
+     * convertToNBase
+     * Inputs: STR0 and STR1; string in the format number_base (or _base for str1)
+     * Outputs: A string in the format number_base, str0 converted to the base specified in str1
+     */
     public String convertToNBase(String str0, String str1);
+    
+    /**
+     * ADD
+     * Inputs: STR0 and STR1; string in the format number_base
+     * Outputs: A string in the format number_base, representing str0 added to str1, with the base from str0
+     */
     public String add(String str0, String str1);
+    
+    /**
+     * ADD
+     * Inputs: STR0 and STR1; string in the format number_base
+     * Outputs: A string in the format number_base, representing str0 subtracted with str1, with the base from str0
+     */
     public String subtract(String str0, String str1);
+    
+    /**
+     * MULTIPLY
+     * Inputs: STR0 and STR1; string in the format number_base
+     * Outputs: A string in the format number_base, representing str0 multiplied to str1, with the base from str0
+     */
     public String multiply(String str0, String str1);
+    
+    /**
+     * DUVIDE
+     * Inputs: STR0 and STR1; string in the format number_base
+     * Outputs: A string in the format number_base, representing str0 divided by str1, with the base from str0
+     */
     public String divide(String str0, String str1);
     
 }
